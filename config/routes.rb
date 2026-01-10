30902c89b66cb6d236899e33aea0c237298c94c8
Cris <245176630+IzarTheGr8@users.noreply.github.com>
2026-01-10 15:50:13 +0100

    add Comments section to post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get "pages/home"
   get "pages/about"
   get "pages/contact"
+
   resources :posts
+  resources :posts do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
