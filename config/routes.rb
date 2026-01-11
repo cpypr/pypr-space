7883afe433c2b374c987b44975dd0537c05957b3
Cris <245176630+IzarTheGr8@users.noreply.github.com>
2026-01-11 13:45:16 +0100

    add header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :posts do
     resources :comments
