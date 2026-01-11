64406036ca60d2150f0160c552cc519fa9d69632
Cris <245176630+IzarTheGr8@users.noreply.github.com>
2026-01-11 12:44:32 +0100

    rewrite Comments section

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
+
   def create
-    @post = Post.find(params[:post_id])
     @comment =  @post.comments.create(params[:comment].permit(:body))
     redirect_to post_path(@post)
+  end
+
+private
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get "pages/about"
   get "pages/contact"
 
-  resources :posts
   resources :posts do
     resources :comments
   end
