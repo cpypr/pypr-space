f93a941a6066e261a24bf0c71ea50735143691c4
Cris <245176630+IzarTheGr8@users.noreply.github.com>
2026-01-20 13:50:23 +0100

    Resolve Rubocop workflow failure

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home', to: 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  get "home", to: "pages#home"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
 
   resources :posts do
     resources :comments
