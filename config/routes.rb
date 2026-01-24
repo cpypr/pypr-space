f0d6a3c0d07b17fbf0a8f2fe7c4adf9f10b27d45
Cris <245176630+IzarTheGr8@users.noreply.github.com>
2026-01-24 13:19:41 +0100

    add authentication #14

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   get "home", to: "pages#home"
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
