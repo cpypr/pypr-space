da83ad162e1b956533df5a9529f4b30e8d6414f0
Cris <245176630+IzarTheGr8@users.noreply.github.com>
2026-02-12 14:34:59 +0100

    Add privacy policy page #24

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "home", to: "posts#index"
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  get "privacy", to: "pages#privacy"
 
   resources :posts do
     resources :comments
