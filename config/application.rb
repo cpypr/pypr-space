ae411c4c75f757b694f19022eee6f159c78faa9d
Cris <245176630+IzarTheGr8@users.noreply.github.com>
2026-02-14 12:41:00 +0100

    bump Rails to 8.1.2

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
